ced873cdf6270eed6ea6d1bc6220a1c7eef75ff5
nikita <zherko.na@phystech.edu>
2022-11-25 16:08:41 +0300

    Add exercise 27

---
+++ ex27/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = NumberFromUser("Введите число: ","Ошибка ввода!");
+int sum = SumOfNumbers(number);
+System.Console.WriteLine($"{number} -> {sum}");
+
+//////////////
+int SumOfNumbers(int FuncNumber)
+{
+    int result = 0;
+    string StrNumber = FuncNumber + String.Empty;
+    for (int i = 0; i < StrNumber.Length; i++)
+    {
+        result = result + number / (int) Math.Pow(10, i) % 10;
+    }
+    return result;
+}
+
+int NumberFromUser(string UserNumber, string MessageError)
+{
+    while (true)
+    {
+        System.Console.WriteLine(UserNumber);
+        if(int.TryParse(Console.ReadLine(), out int CurrentNumber))
+            return CurrentNumber;
+        System.Console.WriteLine(MessageError);
+    }
+}
