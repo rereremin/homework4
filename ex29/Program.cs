0753c673ce7440734da055e836b86f4b08520710
nikita <zherko.na@phystech.edu>
2022-11-25 16:10:18 +0300

    Add exercise 29

---
+++ ex29/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] array = new string[8];
+
+FillArray(array);
+PrintNum(array);
+System.Console.Write(" -> ");
+PrintArray(array);
+
+////////////////////////////////////////////////////////////////////
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Console.ReadLine();
+}
+
+void PrintNum(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        if(i < array.Length - 1)
+            Console.Write(array[i] + ", ");
+        else if(i == array.Length - 1)
+            System.Console.Write(array[i]);
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+        if(i < array.Length - 1)
+            Console.Write(array[i] + ", ");
+        else if(i == array.Length - 1)
+            System.Console.Write(array[i] + "]");
+}
